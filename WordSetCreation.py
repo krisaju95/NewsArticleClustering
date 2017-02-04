726ec55303e7200ced36639c0726abef65155176
akshaykp <akshaykp2895@gmail.com>
2017-02-05 00:59:30 +0530

    WordSetCreation
    Pre processing stage
    o/p: A txt file with words

---
+++ WordSetCreation.py
@@ -0,0 +1,45 @@
+import glob
+import os
+from nltk.corpus import stopwords
+import string
+import nltk, re, pprint
+from nltk import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+
+
+path = "C:\Users\Akshay-PC\Desktop\Articles"
+count = 0
+sw = set()
+sw = stopwords.words('english')
+
+# Creates a Python Set containing all the words found in the documents in path excluding stop words
+        
+def preprocess(sent):
+        f = open("C:\Users\Akshay-PC\Desktop\\Wordset.txt", "a")
+	sent = sent.lower()
+	tokenizer = RegexpTokenizer(r'\w+')
+	tokens = tokenizer.tokenize(sent)
+	filtered_words = [w for w in tokens if not w in sw]
+	for w in filtered_words:
+	   f.write(w + '\n')
+	f.close()
+	
+def createWordSet():
+    for filename in glob.glob(os.path.join(path, '*.txt')):
+        f=open(filename, 'r')
+        op=open("C:\Users\Akshay-PC\Desktop\\temp.txt",'w')
+        for line in f:            
+            line=line.strip().decode("ascii","ignore").encode("ascii")
+            if line=="":
+                continue
+            op.write(line + ' ')
+        op.close()
+
+        fo = open("C:\Users\Akshay-PC\Desktop\\temp.txt", "r")
+        for sent in fo:
+            preprocess(sent)
+        fo.close()
+        
+
+createWordSet()
